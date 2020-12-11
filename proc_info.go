72fc443ece8a84f938143835e773c1d5f0f074fa
吴伋 <wuji@gs-robot.com>
2020-12-11 14:28:23 +0800

    process_info

---
+++ proc_info.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	osDic := make(map[string]interface{}, 0)
+	osDic["goOs"] = runtime.GOOS
+	osDic["arch"] = runtime.GOARCH
+	osDic["mem"] = runtime.MemProfileRate
+	osDic["compiler"] = runtime.Compiler
+	osDic["version"] = runtime.Version()
+	osDic["numGoroutine"] = runtime.NumGoroutine()
+
+	fmt.Println(osDic)
+}
